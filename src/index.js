92a45351d05b10fb18eaa209ed85d3dd9115dbc7
AS81 <sanloginchik@gmail.com>
2021-03-14 00:28:50 +0300

    init: this metod is not going to the result. Save this metod and try new

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-    let bracketsPair = bracketsConfig[0];
     let arrayBrackets = [];
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === bracketsPair[1]) {
-            if (
-                arrayBrackets.length === 0 ||
-                arrayBrackets.pop() != bracketsPair[0]
-            ) {
-                return false;
-            }
-        } else {
-            arrayBrackets.push(str[i]);
+    let result = true;
+    bracketsConfig.forEach((item) => {
+        console.log(item, isStrCorrectOnePairBrackets(str, item));
+        if (!isStrCorrectOnePairBrackets(str, item)) {
+            console.log(`result must be false`);
+            result = false;
         }
+    });
+    console.log(`res is ${result}`);
+    return result;
+
+    function isStrCorrectOnePairBrackets(str, bracketsPair) {
+        //let bracketsPair = bracketsConfig[0];
+        console.log(`str is ${str}, bracketsPair is ${bracketsPair}`);
+        arrayBrackets = [];
+        let correct = true;
+        let bracketIsOpened = false;
+        for (let i = 0; i < str.length; i++) {
+            //if (bracketsPair.indexOf(str[i]) != -1) {
+            if (str[i] === bracketsPair[1]) {
+                bracketIsOpened = false;
+                if (i === 0 || arrayBrackets.pop() != bracketsPair[0]) {
+                    correct = false;
+                }
+            } else {
+                arrayBrackets.push(str[i]);
+                bracketIsOpened = true;
+            }
+            // }
+        }
+        console.log(
+            `arrayB: ${arrayBrackets}, corr = ${correct}, isOpened = ${bracketIsOpened}`
+        );
+        if (bracketIsOpened === true) {
+            return false;
+        } else {
+            return correct;
+        } // return true; //arrayBrackets.length === 0;
     }
-    return arrayBrackets.length === 0;
 };
