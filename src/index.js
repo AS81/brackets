f098b1d96c19cc5146561b3004d6e6806de92309
AS81 <sanloginchik@gmail.com>
2021-03-13 13:33:59 +0300

    feat: work in case when in bracketConfig is only one array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsPair = bracketsConfig[0];
+    let arrayBrackets = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === bracketsPair[1]) {
+            if (
+                arrayBrackets.length === 0 ||
+                arrayBrackets.pop() != bracketsPair[0]
+            ) {
+                return false;
+            }
+        } else {
+            arrayBrackets.push(str[i]);
+        }
+    }
+    return arrayBrackets.length === 0;
+};
